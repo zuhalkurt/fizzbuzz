fd337a3134c239001d96c5a1ead09c530ac15e38
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-01-18 11:53:48 +0000

    Only Print Bong when Divisible by 11

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,18 +6,22 @@ for(let i = 1; i <= 100; i++) {
    
     let result = "";
     
-    if (isDivisibleBy(i, 3)) {
-        result += "Fizz"
+    if (isDivisibleBy(i, 3) && i % 11 !== 0) {
+        result += "Fizz";
     }
-    if( isDivisibleBy(i, 5)) {
+    if( isDivisibleBy(i, 5) && i % 11 !== 0) {
         result += "Buzz"
     }
-    if(isDivisibleBy(i, 7)) {
-        result += "Bang"
+    if(isDivisibleBy(i, 7) && i % 11 !== 0) {
+        result += "Bang";
+    }
+    if (isDivisibleBy(i, 11)) {
+        result += "Bong";
     }
     if (result === ""){
         result = i;
     } 
+    
     console.log(result);  
 }
 
